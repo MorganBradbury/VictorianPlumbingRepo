fee59624c76caf50195a029298c8b8501b401e6b
Morgan Bradbury <Morgan.Bradbury@placesforpeople.co.uk>
2021-06-23 10:45:58 +0100

    Add project files.

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VictorianPlumbing.Models;
+using VictorianPlumbing.Services;
+
+namespace VictorianPlumbing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+
+        private readonly ICreateOrderService _createOrderLayer;
+
+        public OrderController(ICreateOrderService dataLayer)
+        {
+            _createOrderLayer = dataLayer;
+        }
+
+        [Route("[action]")]
+        [HttpPost(Name = "InsertOrder")]
+        public ActionResult<string> InsertOrder([FromBody] Order order)
+        {
+            // If request does not contain correct data, return NotFound page.
+            if(order == null)
+            {
+                return NotFound();
+            }
+
+            // Create order at Services level and return OrderCreated object with data to confirm transaction.
+            var orderCreated = _createOrderLayer.InsertOrder(order);
+            return Ok(orderCreated);
+        }
+
+
+    }
+}
---
+++ Documentation/README.txt
@@ -0,0 +1,36 @@
+﻿Hi Victorian Plumbing, Thank you for considering my application. 
+
+For the API, The following JSON Object was used for my example. All requests were made with Postman as I did not configure the app to use Cors.
+
+{
+    "ID": 1,
+    "Status": "Processing",
+    "Account":{
+        "Email": "morgan.bradbury@test.co.uk",
+        "FirstName": "Morgan",
+        "LastName": "Bradbury",
+        "PhoneNumber": "01772123456"
+    },
+    "Currency": "USD",
+    "OrderedItems": [
+        {
+            "ID": 1,
+            "Name": "Book",
+            "Quantity": 2,
+            "Price": "6.41"
+        },
+        {
+            "ID": 2,
+            "Name": "Pen",
+            "Quantity": 5,
+            "Price": "2.99"
+        }
+    ]
+}
+
+All Controllers can be found in the root project.
+
+All Models can be found in the VictorianPlumbing.Models Class Library
+
+All Services can be found in the VictorianPlumbing.Services Class Library, along with their relevant Interfaces.
+
